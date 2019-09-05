933eb92924374aef54b604afcb97f2291a12b637
Saltanat Degenbaeva <sdegenbaeva@usf.edu>
2019-09-05 19:38:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ISM4300_Deliverable2_Degenbaeva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What grade do you expect in ISM4300?");
+                int grade = int.Parse(Console.ReadLine());
+
+                // Here's the grading criteria according to the syllabus
+                if (grade > 97 && grade < 101)
+                {
+                    Console.WriteLine("You are going to get an A+");
+                }
+                else if (grade > 91 && grade < 98)
+                {
+                    Console.WriteLine("You are going to get an A");
+                }
+                else if (grade > 89 && grade < 92)
+                {
+                    Console.WriteLine("You are going to get an A-");
+                }
+                else if (grade > 87 && grade < 90)
+                {
+                    Console.WriteLine("You are going to get an B+");
+                }
+                else if (grade > 81 && grade < 88)
+                {
+                    Console.WriteLine("You are going to get an B");
+                }
+                else if (grade > 79 && grade < 82)
+                {
+                    Console.WriteLine("You are going to get an B-");
+                }
+                else if (grade > 77 && grade < 80)
+                {
+                    Console.WriteLine("You are going to get an C+");
+                }
+                else if (grade > 71 && grade < 78)
+                {
+                    Console.WriteLine("You are going to get an C");
+                }
+                 else if (grade > 69 && grade < 72)
+                {
+                    Console.WriteLine("You are going to get an C-");
+                }
+                 else if (grade > 67 && grade < 70)
+                {
+                    Console.WriteLine("You are going to get an D+");
+                }
+                else if (grade > 61 && grade < 68)
+                {
+                    Console.WriteLine("You are going to get an D");
+                }
+                else if (grade > 59 && grade < 62)
+                {
+                    Console.WriteLine("You are going to get an D-");
+                }
+              
+                else if (grade < 60 && grade > 0)
+                {
+                    Console.WriteLine("You are going to get an F");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Contact me for any questions!");
+            }
+            
+        }
+    }
+}
