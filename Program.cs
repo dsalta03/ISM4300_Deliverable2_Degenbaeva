b37daf7d6cdacc889911f924fcea3c60191070bd
Saltanat Degenbaeva <sdegenbaeva@usf.edu>
2019-09-12 18:38:58 -0400

    Change-added comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+    Author: Saltanat Degenbaeva
+    Date: 9/12/2019
+    Comments: This C# Console application code demonstrates the use of
+              conditional statements after getting input from users. 
+*/
+using System;
 
 namespace ISM4300_Deliverable2_Degenbaeva
 {
